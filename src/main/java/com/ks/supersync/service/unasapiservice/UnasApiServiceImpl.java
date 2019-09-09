39623995d6b389c900b488b7ea64323381f8fe03
kudog <kudo.gergely@kulcs-soft.hu>
2019-09-09 15:55:39 +0200

    test

--- src/main/java/com/ks/supersync/service/unasapiservice/UnasApiServiceImpl.java
+++ src/main/java/com/ks/supersync/service/unasapiservice/UnasApiServiceImpl.java
@@ -51,7 +51,7 @@ public class UnasApiServiceImpl implements UnasApiService {
     UgyvitelProducts uProducts = new UgyvitelProducts();
 
     Request request = new Request.Builder()
-      .url(unasapiServiceUrl + UnasMServiceEndpoints.GET_PRODUCTS.toString())
+      .url("https://unasapi.herokuapp.com/getproducts_fromunas")
       .get()
       .addHeader("ApiKey", apiKey)
       .build();
