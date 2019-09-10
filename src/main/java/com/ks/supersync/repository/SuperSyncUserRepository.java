8fdf716e6c806d08ccb7eb7d502fb73dbdb32061
kudog <kudo.gergely@kulcs-soft.hu>
2019-09-10 15:09:40 +0200

    sdfgsdfg

--- src/main/java/com/ks/supersync/repository/SuperSyncUserRepository.java
+++ src/main/java/com/ks/supersync/repository/SuperSyncUserRepository.java
@@ -3,9 +3,12 @@ package com.ks.supersync.repository;
 import com.ks.supersync.model.SuperSyncUser;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface SuperSyncUserRepository extends JpaRepository<SuperSyncUser, Integer> {
-
+    
+    @Transactional
     SuperSyncUser findByWebIdentifierAndWebPassword(String WebIdentifier, String WebPassword);
     
 }
