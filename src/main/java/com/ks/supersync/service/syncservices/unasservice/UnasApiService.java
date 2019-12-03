408a512d90fe60368f2ef6d9dc2c081154e38d9a
kudog <kudo.gergely@kulcs-soft.hu>
2019-12-03 14:50:18 +0100

    2134

--- src/main/java/com/ks/supersync/service/syncservices/unasservice/UnasApiService.java
+++ src/main/java/com/ks/supersync/service/syncservices/unasservice/UnasApiService.java
@@ -511,7 +511,7 @@ public class UnasApiService implements SyncService {
       ugyvitelCustomer.centralCountry = unasCustomer.addresses.invoice.country;
       ugyvitelCustomer.centralZip = unasCustomer.addresses.invoice.ZIP;
       ugyvitelCustomer.centralCity = unasCustomer.addresses.invoice.city;
-      ugyvitelCustomer.centralStreet = unasCustomer.addresses.invoice.street;
+      ugyvitelCustomer.centralStreet = unasCustomer.addresses.invoice.streetName;
       ugyvitelCustomer.centralPublicDomain = unasCustomer.addresses.invoice.streetType;
       ugyvitelCustomer.centralStaircase = "";
       ugyvitelCustomer.centralDoor = "";
